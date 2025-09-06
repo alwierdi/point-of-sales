2a08755160282d4d5c2a7b030721167531fe1788
Wikzyy <alwi.erdi4@gmail.com>
2025-09-06 15:01:41 +0700

    setup supabase connection

---
+++ src/configs/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY || "",
+  SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || "",
+};
